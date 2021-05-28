e82797ae63826ce3d53673050f2c51202c797bee
Varsha <dev.varshasoni@gmail.com>
2021-05-28 13:50:49 +0530

    fourth commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { SavedComponent } from './saved/saved.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    SavedComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
